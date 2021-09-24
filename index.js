7c1be5addae341279c154ef2b20871f11cf37a12
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-24 00:37:05 +0000

    final solution for ISS Spotter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,10 +8,30 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP('135.12.178.128', (error, data) => {
+// fetchCoordsByIP('135.12.178.128', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// fetchISSFlyOverTimes({latitude: 45.5154, longitude: -73.5822}, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log(data);
+  // success, print out the deets!
+  for (let i = 0; i < passTimes.length; i++) {
+    let date = new Date(0);
+    date.setUTCSeconds(passTimes[0].risetime);
+    console.log(`Next pass at ${date} for ${passTimes[i].duration} seconds!`);
+  }
 });
--- iss.js
+++ iss.js
@@ -9,7 +9,16 @@
 
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+// const fetchMyIP = function (callback) {
+// };
+
+// const fetchCoordsByIP = function (ip, callback) {
+// };
+
+// const fetchISSFlyOverTimes = function (coords, callback) {
+// };
+
+const nextISSTimesForMyLocation = function(callback) {
   // use request to fetch IP address from JSON API
   request("https://api.ipify.org/?format=json", (error, response, body) => {
     // if there is error, pass the callback function with the error
@@ -26,31 +35,44 @@ const fetchMyIP = function(callback) {
 
     //parse the string and callback the data
     const ip = JSON.parse(body).ip;
-    callback(null, ip);
+
+    // use request to fetch coordinates using IP
+    request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+      // if there is error, pass the callback function with the error
+      if (error) {
+        return callback(error, null);
+      }
+
+      // if non-200 status, assume server error
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+        callback(msg, null);
+        return;
+      }
+
+      //input the latitude and longitude into an object
+      const latitude = JSON.parse(body).latitude;
+      const longitude = JSON.parse(body).longitude;
+      const coordinates = { latitude, longitude };
+
+      // use request to fetch coordinates using IP
+      request(`https://iss-pass.herokuapp.com/json/?lat=${coordinates.latitude}&lon=${coordinates.longitude}`, (error, response, body) => {
+        // if there is error, pass the callback function with the error
+        if (error) {
+          return callback(error, null);
+        }
+
+        // if non-200 status, assume server error
+        if (response.statusCode !== 200) {
+          const msg = `Status Code ${response.statusCode} when fetching ISS pass time. Response: ${body}`;
+          callback(msg, null);
+          return;
+        }
+        const passTimes = JSON.parse(body).response;
+        callback(null, passTimes);
+      });
+    });
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
-  // use request to fetch coordinates using IP
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
-    // if there is error, pass the callback function with the error
-    if (error) {
-      return callback(error, null);
-    }
-
-    // if non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
-      callback(msg, null);
-      return;
-    }
-
-    //input the latitude and longitude into an object
-    const latitude = JSON.parse(body).latitude;
-    const longitude = JSON.parse(body).longitude;
-    const coordinates = {latitude, longitude};
-    callback(null, coordinates);
-  });
-};
-
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { nextISSTimesForMyLocation };
